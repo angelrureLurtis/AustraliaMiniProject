9625c6faa2206cfea30e7217b93160e5f88f1ff6
Angel Ruiz <angelrure@gmail.com>
2020-01-24 18:53:49 +0100

    update geosearch

--- geolocalization/queries.py
+++ geolocalization/queries.py
@@ -18,7 +18,8 @@ import requests
 import pandas as pd
 
 def search_by_distance(location, distance, distance_type, 
-                       include_indicators_with_assets=True, folder_name='Any'):
+                       include_indicators_with_assets=True, folder_name='Any',
+                       indicator=None):
     """
 
     General high level function that searches the indicators inside a radius of 
@@ -47,10 +48,27 @@ def search_by_distance(location, distance, distance_type,
 
     results = _search_by_distance(**settings, location=location, distance=distance, 
                                   distance_type=distance_type, folder_name=folder_name,
-                                  include_indicators_with_assets=include_indicators_with_assets)
+                                  include_indicators_with_assets=include_indicators_with_assets,
+                                  indicator=indicator)
     return results
 
-def return_all_national_indicators(location='Sydney'):
+def indicators_given_asset(asset_name):
+    
+    settings = _retrieve_query_settings(ds='search')
+    
+    url = settings['url']
+    path = settings['path']
+    port = settings['port']
+    
+    results = requests.get(f'{url}:{port}/{path}', params={'asset':asset_name,
+                                                         'model':'Actual',
+                                                         'output_format':'json'})
+    
+    return results
+
+
+
+def return_all_national_indicators(location='Sydney', parse_json=False):
     """
     
     General function to retrieve all the national indicators from a Country.
@@ -72,6 +90,7 @@ def return_all_national_indicators(location='Sydney'):
     
     data = {
         'location_name':location,
+        'distance_type':'km',
         'only_nationals':True,
     }
     
@@ -83,17 +102,107 @@ def return_all_national_indicators(location='Sydney'):
 
     for tag in json_out['responses']:
         for result in request.json()['responses'][tag]:
+            results[tag] = {}
             sk = result['search_key']
             sk_parsed = _parse_search_key_info(sk)
             settings = _retrieve_query_settings(simple=True, add_method=False)
             result2 = _match_key_to_actual(**settings, sk=sk)
-            results[sk] = sk_parsed
-            results[sk]['responses'] = result2.json()['responses']
-	    
+            results[tag] = sk_parsed
+            results[tag].update(result2)
+
+        if parse_json:
+            
+            if tag in results.keys():
+                results[tag] = parse_inner_lists(results[tag], 
+                                                ['actual_value', 'date'], 
+                                                'responses', 'all')
+
+    if parse_json:
+
+        results = merge_keys(results, 'folder_name')
+
     return results
 
+def parse_inner_lists(json_data, cols_from_nested, target_nested_column, to_keep_columns):
+    """
+    Parses JSON data that contains a nested column with a list of dicionaries. 
+    This is inteded to make much easier the conversion to a pandas DataFrame.
+    
+    Args:
+        json_data(dict): a dictionary containing one nested value.
+        cols_from_nested(list): a list containing the values to extract from
+                                the nested dictionaries.
+        target_nested_column(str): the name of the value that contains a list
+                                   of dicionaries.
+        to_keep_columns(list or 'all'): either a list containing the name of 
+                                        the unnested columns to keep or the
+                                        string 'all' which means to keep them
+                                        all. If list, do not include the value
+                                        that has the nested values.
+    
+    Returns:
+        
+        A dictionary with the nested value unnested. Ideally to be feed 
+        directly into a Pandas DataFrame.
+    
+    Usage example:
+    
+        parsed = parse_inner_lists(request.json(), ['actual_value', 'date'], 
+                                   'responses', 'all')
 
-def _retrieve_query_settings(simple=False, add_method=False):
+        clean = pd.DataFrame(parsed)
+        
+    """
+    
+    results = []
+
+    if to_keep_columns=='all':
+        
+        to_keep_columns = [col for col in json_data.keys() if col != target_nested_column]
+        
+    for response in json_data[target_nested_column]:
+        
+        r = {}
+
+        for col in cols_from_nested:
+
+            r.update({
+                col:response[col],
+            })
+
+        for col in to_keep_columns:
+
+            r.update({col:json_data[col]})
+
+        results.append(r)
+    
+    return results
+
+def merge_keys(dictionary, key_name):
+    """
+    Takes the higher level key of a dicionary and uses it
+    as a value of a inner list of dicionaries. Useful
+    to convert the dictionary into a pandas DataFrame.
+    
+    Args
+    
+        dictionary(dict): The dictionary which should have
+                          a list of dictionaries in each
+                          highest level value.
+        key_name(str): The name to give to the highest level
+                       keys once inside the new dictionaries.
+    """
+    
+    results = []
+    
+    for key in dictionary.keys():
+        for dict_ in dictionary[key]:
+            dict_.update({key_name:key})
+            results.append(dict_)
+    
+    return results
+
+def _retrieve_query_settings(simple=False, add_method=False, ds=False):
     """
     Internal function to generate the necessary settings to do the query
     calls. 
@@ -105,6 +214,9 @@ def _retrieve_query_settings(simple=False, add_method=False):
         add_method(bool): Whether to add the method to call the API
                           in the settings. It is usually not necesssary
                           so it is set to False.
+        ds(string): Whether to use the DataScience API instead of the 
+                    geo. If evaluates to True must be a string that
+                    contains the specific endpoint.
 
     Returns:
 
@@ -114,10 +226,12 @@ def _retrieve_query_settings(simple=False, add_method=False):
     
     url = 'http://ec2-13-229-144-6.ap-southeast-1.compute.amazonaws.com'
     
-    if simple:
+    if simple and not ds:
          path = 'ui_data_simple_query'
-    else:
+    elif not simple and not ds:
         path = 'ui_geosearch_query'
+    elif ds:
+        path = 'datascience/'+ds
    
     port = '5000'
     
@@ -157,12 +271,12 @@ def _match_key_to_actual(url, port, path, sk):
     
     data = {
     'search_key': sk,
-    'sudo':False, # para cojer solo actuals
+    'sudo':False, # this means only actuals are used.
     }
     
     request = requests.post(f'{url}:{port}/{path}', json=data)
-    
-    return request
+
+    return request.json()
 
 def _parse_search_key_info(search_key):
     """
@@ -190,7 +304,8 @@ def _parse_search_key_info(search_key):
 
 
 def _search_by_distance(url, port, path, location, distance, distance_type, 
-                        include_indicators_with_assets, folder_name):
+                        include_indicators_with_assets, folder_name,
+                        indicator=False):
     """
 
     Internal function that calls the API for the geolocalization search.
@@ -225,8 +340,17 @@ def _search_by_distance(url, port, path, location, distance, distance_type,
         'include_indicators_with_assets': include_indicators_with_assets
     }
     
+    if indicator:
+        data.update({'indicator_name_id':indicator})
+
     request = requests.post(f'{url}:{port}/{path}', json=data)
-    
+
+    try:    
+        output = request.json()
+    except:
+        print('Request failed, see message error: ')
+        print(request.content)
+        return output
 
     for result in request.json()['responses'][folder_name]:
                 
@@ -235,6 +359,6 @@ def _search_by_distance(url, port, path, location, distance, distance_type,
         settings = _retrieve_query_settings(simple=True, add_method=False)
         result2 = _match_key_to_actual(**settings, sk=sk)
         results[sk] = sk_parsed
-        results[sk]['responses'] = result2.json()['responses'] 
+        results[sk]['responses'] = result2['responses'] 
     
     return results
